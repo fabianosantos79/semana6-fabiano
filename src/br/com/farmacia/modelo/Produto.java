cf117a0f28a9cfd346babbc9deb2257413ff4cb1
Fabiano <fabiano.ssantos79@gmail.com>
2024-02-26 01:05:54 -0300

    5 - Criar a classe de Produto

--- src/br/com/farmacia/modelo/Produto.java
+++ src/br/com/farmacia/modelo/Produto.java
@@ -1,4 +1,41 @@
 package br.com.farmacia.modelo;
 
 public class Produto {
+    private int id;
+    private double preco;
+    private String nome;
+    private String fabricante;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
 }
